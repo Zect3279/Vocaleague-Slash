38fdd6df24d1ce5a8e45fe2e9ad22703b8cc8d5e
zect <s.a.s.jishu@gmail.com>
2021-03-25 21:52:50 +0900

    [Fix]: コメント化

--- lib/point.py
+++ lib/point.py
@@ -21,20 +21,20 @@ class Point():
 
 
 
-    async def ranking(self, ctx: SlashContext) -> None:
-        users_ranking = await self.db.get_user_rankings()
-        ranking_message = "```\n"
-
-        for user_ranking in users_ranking:
-            user = self.bot.get_user(user_ranking[0])
-            user_data = user_ranking[0]
-
-            ranking_message += f"{user_ranking[1]}位: {user_data.name}, Point: {user_data.point}\n"
-
-        if not discord.utils.find(lambda u: u[0].id == ctx.author.id, users_ranking):
-            if rank := await self.db.get_user_ranking(ctx.author.id):
-                user_data = await self.bot.db.get_user(ctx.author)
-                ranking_message += f"\n{rank}位: {user_data.name}, Point: {user_data.point}"
-
-        txt = ranking_message + "```"
-        return txt
+    # async def ranking(self, ctx: SlashContext) -> None:
+    #     users_ranking = await self.db.get_user_rankings()
+    #     ranking_message = "```\n"
+    #
+    #     for user_ranking in users_ranking:
+    #         user = self.bot.get_user(user_ranking[0])
+    #         user_data = user_ranking[0]
+    #
+    #         ranking_message += f"{user_ranking[1]}位: {user_data.name}, Point: {user_data.point}\n"
+    #
+    #     if not discord.utils.find(lambda u: u[0].id == ctx.author.id, users_ranking):
+    #         if rank := await self.db.get_user_ranking(ctx.author.id):
+    #             user_data = await self.bot.db.get_user(ctx.author)
+    #             ranking_message += f"\n{rank}位: {user_data.name}, Point: {user_data.point}"
+    #
+    #     txt = ranking_message + "```"
+    #     return txt
